550b241d14fdcae57319830da249cbaf2e980780
mario <mario28.ce@gmail.com>
2025-06-15 00:08:44 -0600

    Cambios en el envio de mensajes JSON

--- Disk_Controller_Server/ConeccionHTTP.cpp
+++ Disk_Controller_Server/ConeccionHTTP.cpp
@@ -1,113 +1,155 @@
-#include "ConeccionHTTP.h"
-#include "json.hpp"
-using json = nlohmann::json;
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma comment(lib,"ws2_32.lib")
+#include "ConeccionHTTP.h"
+#include "json.hpp"
 #include <WinSock2.h>
 #include <string>
 #include <iostream>
-#define addr "127.0.0.1" // Direccion del server con PDF App
-#define port 8080 // Puerto con PDF App
-using namespace std;
+using json = nlohmann::json;
+#define addr "127.0.0.1"
+#define port 8080
 
 void ConeccionHTTP::run() {
-	SOCKET wsocket;
-	SOCKET new_wsocket;
-	WSADATA wsaData;
-	struct sockaddr_in server;
-	int server_len;
-	const int BUFFER_SIZE = 30720;
+    SOCKET wsocket;
+    SOCKET new_wsocket;
+    WSADATA wsaData;
+    struct sockaddr_in server;
+    int server_len;
+    const int BUFFER_SIZE = 30720;
 
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cout << "Error de Inicialización";
-	}
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cout << "Error de Inicialización: " << WSAGetLastError() << std::endl;
+        return;
+    }
 
-	// Crear socket
-	wsocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (wsocket == INVALID_SOCKET) {
-		std::cout << "Socket no creado";
-	}
+    wsocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (wsocket == INVALID_SOCKET) {
+        std::cout << "Socket no creado: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return;
+    }
 
-	// Configurar direccion
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr(addr);
-	server.sin_port = htons(port);
-	server_len = sizeof(server);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(addr);
+    server.sin_port = htons(port);
+    server_len = sizeof(server);
 
+    if (bind(wsocket, (SOCKADDR*)&server, server_len) == SOCKET_ERROR) {
+        std::cout << "Error en bind: " << WSAGetLastError() << std::endl;
+        closesocket(wsocket);
+        WSACleanup();
+        return;
+    }
 
-	// Escuchar en la direccion
-	if (listen(wsocket, 20) != 0) {
-		std::cout << "No se puede escuchar";
-	}
+    if (listen(wsocket, 20) != 0) {
+        std::cout << "No se puede escuchar: " << WSAGetLastError() << std::endl;
+        closesocket(wsocket);
+        WSACleanup();
+        return;
+    }
 
-	char buff[BUFFER_SIZE] = { 0 };
+    char buff[BUFFER_SIZE] = { 0 };
+    while (true) {
+        new_wsocket = accept(wsocket, (SOCKADDR*)&server, &server_len);
+        if (new_wsocket == INVALID_SOCKET) {
+            std::cout << "No se pudo conectar: " << WSAGetLastError() << std::endl;
+            continue;
+        }
 
-	while (true) {
-		// Aceptar clientes
-		new_wsocket = accept(wsocket, (SOCKADDR*)&server, &server_len);
-		if (new_wsocket == INVALID_SOCKET) {
-			std::cout << "No se pudo conectar";
-			continue;
-		}
+        std::string request;
+        memset(buff, 0, BUFFER_SIZE);
+        int bytes;
+        while ((bytes = recv(new_wsocket, buff, BUFFER_SIZE - 1, 0)) > 0) {
+            buff[bytes] = '\0';
+            request += buff;
+            if (request.find("\r\n\r\n") != std::string::npos) {
+                break;
+            }
+        }
+        if (bytes < 0) {
+            std::cout << "No se pudo leer request: " << WSAGetLastError() << std::endl;
+            closesocket(new_wsocket);
+            continue;
+        }
 
-		// Leer request
-		int bytes = recv(new_wsocket, buff, BUFFER_SIZE, 0);
-		if (bytes < 0) {
-			std::cout << "no se pudo leer request";
-			closesocket(new_wsocket);
-			continue;
-		}
+        std::string method = request.substr(0, request.find(" "));
+        if (method != "POST") {
+            std::cout << "Método no permitido: " << method << std::endl;
+            std::string errorResponse =
+                "HTTP/1.1 405 Method Not Allowed\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: 23\r\n"
+                "Connection: close\r\n"
+                "\r\n"
+                "Solo se permite POST";
+            send(new_wsocket, errorResponse.c_str(), errorResponse.size(), 0);
+            closesocket(new_wsocket);
+            continue;
+        }
 
-		// Convertir buffer a string
-		std::string request(buff, bytes);
+        size_t pos = request.find("\r\n\r\n");
+        std::string body = (pos != std::string::npos) ? request.substr(pos + 4) : "";
+        std::cout << "Cuerpo recibido: [" << body << "]" << std::endl;
 
-		// Buscar dónde empieza el cuerpo JSON (después de "\r\n\r\n")
-		size_t pos = request.find("\r\n\r\n");
-		std::string body = (pos != std::string::npos) ? request.substr(pos + 4) : "";
+        json responseJson;
+        responseJson["status"] = "active";
+        responseJson["mensaje"] = u8"\u00A1Bienvenido al servidor Disk Controller!"; // Cadena UTF-8 explícita
+        if (body.empty()) {
+            std::cout << "Cuerpo de la solicitud vacío" << std::endl;
+            std::string errorResponse =
+                "HTTP/1.1 400 Bad Request\r\n"
+                "Content-Type: application/json\r\n"
+                "Content-Length: 0\r\n"
+                "Connection: close\r\n"
+                "\r\n";
+            send(new_wsocket, errorResponse.c_str(), errorResponse.size(), 0);
+            closesocket(new_wsocket);
+            continue;
+        }
 
-		// Parsear JSON si existe
-		json j;
-		if (!j["nombre"].is_null()) {
-			std::cout << "Nombre: " << j["nombre"] << std::endl;
-		}
-		try {
-			j = json::parse(body);
-			std::cout << "JSON recibido: " << j.dump() << std::endl;
-		}
-		catch (json::parse_error& e) {
-			std::cout << "Error al parsear JSON: " << e.what() << std::endl;
-			// Puedes responder con un error HTTP aquí si quieres
-		}
+        try {
+            json j = json::parse(body);
+            std::cout << "JSON recibido: " << j.dump() << std::endl;
+            if (!j["nombre"].is_null()) {
+                std::cout << "Nombre: " << j["nombre"] << std::endl;
+            }
+            responseJson["eco"] = j;
+        }
+        catch (json::parse_error& e) {
+            std::cout << "Error al parsear JSON: " << e.what() << std::endl;
+            std::string errorResponse =
+                "HTTP/1.1 400 Bad Request\r\n"
+                "Content-Type: application/json\r\n"
+                "Content-Length: 0\r\n"
+                "Connection: close\r\n"
+                "\r\n";
+            send(new_wsocket, errorResponse.c_str(), errorResponse.size(), 0);
+            closesocket(new_wsocket);
+            continue;
+        }
 
-		// Construir respuesta JSON
-		json responseJson;
-		responseJson["status"] = "active";
-		responseJson["mensaje"] = "¡Bienvenido al servidor Disk Controller!";
-		responseJson["eco"] = j;
+        std::string responseBody = responseJson.dump();
+        std::string serverMessage =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: application/json\r\n"
+            "Content-Length: " + std::to_string(responseBody.size()) + "\r\n"
+            "Connection: close\r\n"
+            "\r\n" +
+            responseBody;
 
-		std::string responseBody = responseJson.dump();
+        int totalBytesSent = 0;
+        int messageSize = serverMessage.size();
+        while (totalBytesSent < messageSize) {
+            int bytesSent = send(new_wsocket, serverMessage.c_str() + totalBytesSent, messageSize - totalBytesSent, 0);
+            if (bytesSent <= 0) {
+                std::cout << "Error al enviar respuesta: " << WSAGetLastError() << std::endl;
+                break;
+            }
+            totalBytesSent += bytesSent;
+        }
+        closesocket(new_wsocket);
+    }
 
-		// Construir encabezado HTTP para respuesta JSON
-		std::string serverMessage =
-			"HTTP/1.1 200 OK\r\n"
-			"Content-Type: application/json\r\n"
-			"Content-Length: " + std::to_string(responseBody.size()) + "\r\n"
-			"Connection: close\r\n"
-			"\r\n" +
-			responseBody;
-
-		int totalBytesSent = 0;
-		int messageSize = serverMessage.size();
-		while (totalBytesSent < messageSize) {
-			int bytesSent = send(new_wsocket, serverMessage.c_str(), serverMessage.size(), 0);
-			if (bytesSent < 0) {
-				cout << "no se envio respuesta";
-			}
-			totalBytesSent += bytesSent;
-		}
-		closesocket(new_wsocket);
-	}
-
-	WSACleanup();
-
+    closesocket(wsocket);
+    WSACleanup();
 }
